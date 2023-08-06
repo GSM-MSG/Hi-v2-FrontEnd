18a0cd6b301af65b18028db3072f6952ef721c85
unknown <momlin069@gmail.com>
2023-08-06 21:51:49 +0900

    add change login, logout Text

--- assets/svg/HiLogo.tsx
+++ assets/svg/HiLogo.tsx
@@ -1,8 +1,8 @@
 function HiLogo() {
   return (
     <svg
-      width='30'
-      height='38'
+      width='40'
+      height='40'
       viewBox='0 0 30 38'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
