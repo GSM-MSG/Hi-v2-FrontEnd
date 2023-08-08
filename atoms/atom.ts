cf124bcfc0458c6622c8acf0409bd0094c0df7ea
SeoJumee <official.jumee@gmail.com>
2023-08-08 23:57:52 +0900

    fix hasLogin state

--- atoms/atom.ts
+++ atoms/atom.ts
@@ -1,5 +1,9 @@
+import { getStorage } from '@/utils/Storage'
 import { atom } from 'recoil'
 
-export const IsModal = atom<boolean>({ key: 'IsModal', default: true })
+export const IsModal = atom<boolean>({ key: 'IsModal', default: false })
 export const ModalPage = atom<number>({ key: 'ModalPage', default: 1 })
-export const HasLogin = atom<boolean>({ key: 'HasLogin', default: false })
+export const HasLogin = atom<boolean>({
+  key: 'HasLogin',
+  default: getStorage('hi_accessToken') ? true : false,
+})
