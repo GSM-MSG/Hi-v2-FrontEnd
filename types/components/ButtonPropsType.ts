6200e10ab8f8cce1f31131a2f6f3075eb1a3b540
SeoJumee <official.jumee@gmail.com>
2023-08-10 21:27:35 +0900

    update component props type name

--- types/components/ButtonPropsType.ts
+++ types/components/ButtonPropsType.ts
@@ -1,4 +1,4 @@
-export type ButtonType = {
+export type ButtonPropsType = {
   children: React.ReactNode
   width?: string
   height?: string
--- types/components/PageContainerPropsType.ts
+++ types/components/PageContainerPropsType.ts
@@ -1,8 +1,9 @@
-export type PageContainerType = {
+export type PageContainerPropsType = {
   children: React.ReactNode
   display?: string
   alignItems?: string
   justifyContent?: string
   paddingTop?: string
   paddingBottom?: string
+  background?: string
 }
