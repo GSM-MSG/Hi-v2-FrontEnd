28d14bf357ba05ea0d32fb96c28b4fa1c9f3adda
unknown <momlin069@gmail.com>
2023-08-26 20:27:54 +0900

    :lipstick: Update reservation page container

--- components/ReservationPage/style.ts
+++ components/ReservationPage/style.ts
@@ -1,13 +1,5 @@
 import styled from '@emotion/styled'
 
-export const ReservationPageContainer = styled.div`
-  width: 100vw;
-  min-height: calc(100vh - 4rem);
-  height: 100%;
-  background-color: #f5f5f5;
-  padding: 8vh 15vw 4vh 15vw;
-`
-
 export const ReservationTitle = styled.h2`
   font-size: 1.4rem;
   font-weight: 600;
@@ -18,7 +10,3 @@ export const ReservationTableContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `
-
-// export const ReserVationTableBox = styled.div`
-
-// `
