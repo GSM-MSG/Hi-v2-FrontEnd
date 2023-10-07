911a495c739c7c353ab6ba28e58e6cb696aa8deb
unrin <momlin069@gmail.com>
2023-10-07 13:38:32 +0900

    :zap: Improve common textarea

--- components/Common/Textarea/index.tsx
+++ components/Common/Textarea/index.tsx
@@ -1,17 +1,20 @@
 import * as S from './style'
 import { TextareaPropsType } from '@/types/components/TextAreaProps'
 
-export default function Textarea(props: TextareaPropsType) {
+export default function Textarea({
+  height,
+  borderColor,
+  fontSize,
+  margin,
+  ...rest
+}: TextareaPropsType) {
   return (
     <S.Textarea
-      value={props.value}
-      height={props.height}
-      borderColor={props.borderColor}
-      placeholder={props.placeholder}
-      fontSize={props.fontSize}
-      margin={props.margin}
-      onChange={props.onChange}
-      name={props.name}
+      height={height}
+      borderColor={borderColor}
+      fontSize={fontSize}
+      margin={margin}
+      {...rest}
     />
   )
 }
