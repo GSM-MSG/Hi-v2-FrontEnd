c986b7aa147328e2952d4c3491b586de96f131f9
unrin <momlin069@gmail.com>
2023-10-25 23:43:03 +0900

    :zap: Add reservation buttons

--- components/ReservationPage/index.tsx
+++ components/ReservationPage/index.tsx
@@ -8,11 +8,26 @@ import ReservationTableItem from './ReservationTableItem'
 import * as S from './style'
 import useModal from '@/hooks/useModal'
 import PlaceSelect from '@/modals/PlaceSelect'
+import Button from '../common/Button'
+import { GetRoleTypes } from '@/types/components/GetRoleTypes'
 
 function ReservationPage() {
   const reservationPlace = useRecoilValue(ReservationPlace)
   const { fetch, data } = useFetch<ReservationDataType[]>({
     url: `/homebase?period=${reservationPlace.period}&floor=${reservationPlace.floor}`,
+    method: 'get',
+  })
+
+  const today: Date = new Date()
+
+  const { fetch: deleteTable } = useFetch({
+    url: '/reservation/kill-all',
+    method: 'delete',
+    successMessage: '예약 테이블을 모두 삭제했습니다.',
+  })
+
+  const { fetch: fetchRole, data: roleDate } = useFetch<GetRoleTypes>({
+    url: '/user/my-role',
     method: 'get',
   })
 
@@ -25,6 +40,10 @@ function ReservationPage() {
   }, [fetch])
 
   useEffect(() => {
+    ;(async () => await fetchRole())()
+  }, [fetchRole])
+
+  useEffect(() => {
     setArr([1, 2, 3, 4, 5])
     setArr((prev: any) =>
       prev.map(
@@ -33,13 +52,55 @@ function ReservationPage() {
     )
   }, [data])
 
-  useEffect(() => {
-    openModal(<PlaceSelect />)
-  }, [])
-
   return (
     <PageContainer paddingTop='8vh' paddingBottom='5vh' background='#ffffff'>
-      <S.ReservationTitle>예약현황</S.ReservationTitle>
+      <S.ReservationTitleBox>
+        <S.ReservationTitle>
+          <h2>예약현황</h2>
+          <div>
+            {today.getFullYear()}.{today.getMonth().toString().padStart(2, '0')}
+            .{today.getDay().toString().padStart(2, '0')}
+          </div>
+          <div>
+            {reservationPlace.floor}층 &#12685; {reservationPlace.period}교시
+          </div>
+        </S.ReservationTitle>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Button
+            width='3.8rem'
+            height='1.8rem'
+            border='1px solid #0066ff'
+            borderRadius='8px'
+            color='#0066ff'
+            fontSize='0.81rem'
+            fontWeight='500'
+            background='none'
+            hoverBackground='#0066ff'
+            hoverColor='#ffffff'
+            onClick={() => openModal(<PlaceSelect />)}
+          >
+            상세조회
+          </Button>
+          {roleDate?.role.includes('ROLE_ADMIN') && (
+            <Button
+              width='3.8rem'
+              height='1.8rem'
+              border='1px solid #FF002E'
+              borderRadius='8px'
+              color='#FF002E'
+              fontSize='0.81rem'
+              fontWeight='500'
+              background='none'
+              hoverBackground='#FF002E'
+              hoverColor='#ffffff'
+              style={{ marginLeft: '0.3rem' }}
+              onClick={async () => await deleteTable()}
+            >
+              전체삭제
+            </Button>
+          )}
+        </div>
+      </S.ReservationTitleBox>
       <S.ReservationTableContainer>
         {arr.slice(0, reservationPlace.floor === 3 ? 5 : 4).map((item, idx) => (
           <ReservationTableItem
