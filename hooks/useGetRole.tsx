449dbf8983be805cd4bdbec6f89887f803b3361e
unrin <momlin069@gmail.com>
2023-12-06 11:05:49 +0900

    :bug: Fix get autority error

--- hooks/useGetRole.tsx
+++ hooks/useGetRole.tsx
@@ -2,6 +2,8 @@ import useFetch from './useFetch'
 import { useEffect } from 'react'
 import { useGetRoleReturnType } from '@/types/hooks'
 import { GetRoleType } from '@/types/components'
+import { useRecoilValue } from 'recoil'
+import { HasLogin } from '@/atoms/atom'
 
 export default function useGetRole(): useGetRoleReturnType {
   const { fetch, data } = useFetch<GetRoleType>({
@@ -9,13 +11,15 @@ export default function useGetRole(): useGetRoleReturnType {
     method: 'get',
   })
 
+  const hasLogin = useRecoilValue(HasLogin)
+
   const hasRole = (role: string) => {
     return data?.role.includes(role)
   }
 
   useEffect(() => {
     ;(async () => await fetch())()
-  }, [])
+  }, [hasLogin])
 
   return {
     isAdmin: hasRole('ROLE_ADMIN'),
