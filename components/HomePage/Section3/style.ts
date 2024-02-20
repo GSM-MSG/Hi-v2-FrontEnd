c6ac780558db063b2a1cd589f6c96b307dd04a24
이운린 <105215297+likegitman@users.noreply.github.com>
2024-02-21 01:10:02 +0900

    Update home section 3

--- components/HomePage/Section3/style.ts
+++ components/HomePage/Section3/style.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 
 export const HomeSection3 = styled.div`
+  position: relative;
   width: 100vw;
   display: flex;
   flex-direction: column;
@@ -22,21 +23,41 @@ export const DescriptionContainer = styled.div`
   padding-right: 240px;
 
   img {
+    z-index: 9;
     box-shadow: 0px 8px 30px 0px rgba(112, 144, 176, 0.12);
   }
+`
+
+export const Circle1 = styled.div`
+  position: absolute;
+  width: 597px;
+  height: 595px;
+  background-color: #f0f5ff;
+  border-radius: 50%;
+  top: 9%;
+  left: -14%;
+`
+
+export const Circle2 = styled.div`
+   position: absolute;
+  width: 553px;
+  height: 551px;
+  background-color: #f0f5ff;
+  border-radius: 50%;
+  bottom: 17%;
+  right: 20%;
 `
 
 export const IntroductoryTextBox3 = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
+  align-items: flex-start;
   gap: 10px;
 
   h2 {
     font-size: 2.3rem;
     color: #000000;
     font-weight: 700;
-    text-align: right;
     white-space: nowrap;
 
     span {
