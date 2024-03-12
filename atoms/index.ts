403f64f7b2d96c773f6b1a775c35763a4803e7ae
이운린 <105215297+likegitman@users.noreply.github.com>
2024-03-12 09:37:12 +0900

    :bug: Fix role recoil state default

--- atoms/index.ts
+++ atoms/index.ts
@@ -36,7 +36,7 @@ export const SelectedUser = atom<UserItemType>({
     classNum: 1,
     number: 1,
     profileImageUrl: '',
-    role: '',
+    role: 'ROLE_STUDENT',
     useStatus: 'AVAILABLE',
   },
 })
