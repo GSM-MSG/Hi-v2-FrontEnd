aef40a04617cc3ffbd5a2a9a2cf8cf521ccde283
SeoJumee <official.jumee@gmail.com>
2024-03-22 23:49:34 +0900

    💄 Update my page

--- components/MyPage/index.tsx
+++ components/MyPage/index.tsx
@@ -1,20 +1,17 @@
-import { BackArrowIcon, UserProfile } from '@/assets'
-import { MyPageType } from '@/types'
+import { UserProfile } from '@/assets'
 import Image from 'next/image'
-import { useState } from 'react'
 import * as S from './style'
 import { useQuery } from '@tanstack/react-query'
 import { AxiosResponse } from 'axios'
 import { get, userQueryKeys, userUrl } from '@/apis'
+import ReservationItem from '../ReservationItem'
+import { MyPageType } from '@/types'
 
 export default function MyPage() {
-  const [showDetailName, setShowDetailName] = useState(false)
-
   const { data } = useQuery<AxiosResponse<MyPageType>>({
     queryKey: userQueryKeys.my(),
     queryFn: () => get(userUrl.my()),
   })
-
   const { useStatus, profileImageUrl, name, email, reservations } =
     data?.data || {}
 
@@ -47,35 +44,10 @@ export default function MyPage() {
       <S.ReservationWrapper>
         <S.ReservationTitle>
           <span>나의 예약 현황</span>
-          {reservations && <span>예약 중</span>}
+          {reservations?.length !== 0 && <span>예약 중</span>}
         </S.ReservationTitle>
         {reservations?.map((i, idx) => (
-          <S.Reservation key={idx}>
-            <div>
-              <span className='tableNum'>
-                {i.homeBase.homeBaseNumber}번 테이블
-              </span>
-            </div>
-            <div>
-              <S.NameWrapper
-                showDetailName={showDetailName}
-                onClick={() => setShowDetailName((prev) => !prev)}
-              >
-                <S.Name>
-                  {i.users[0].name} 외 {i.users.length - 1} 명
-                  <BackArrowIcon />
-                </S.Name>
-                {showDetailName && (
-                  <S.DetailName>
-                    {i.users.map(({ name }) => name).join(', ')}
-                  </S.DetailName>
-                )}
-              </S.NameWrapper>
-              <span className='info'>
-                {i.homeBase.period}교시 · {i.homeBase.floor}F
-              </span>
-            </div>
-          </S.Reservation>
+          <ReservationItem reservation={i} key={idx} />
         ))}
       </S.ReservationWrapper>
     </S.PageContainer>
