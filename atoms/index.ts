c63e92ada544a4b3a38c7ea2d2e71bb138915b2b
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-17 14:27:15 +0900

    :zap: Update period time

--- atoms/index.ts
+++ atoms/index.ts
@@ -2,11 +2,36 @@ import { UserItemType } from '@/types/components'
 import { ReactNode } from 'react'
 import { atom } from 'recoil'
 
+const time7 = new Date()
+time7.setHours(15, 30)
+const time8 = new Date()
+time8.setHours(16, 40)
+const time9 = new Date()
+time9.setHours(17, 40)
+const time10 = new Date()
+time10.setHours(19, 30)
+const currentTime = new Date()
+const is7Period = new Date().getDay() === 1 && currentTime < time7
+const is8Period = currentTime < time8
+const is9Period = currentTime < time9
+const is10Period = currentTime < time10
+
 export const IsModal = atom<ReactNode>({ key: 'IsModal', default: null })
 
 export const ReservationPlace = atom<{ floor: number; period: number }>({
   key: 'Place',
-  default: { floor: 2, period: new Date().getDay() === 1 ? 7 :  8 },
+  default: {
+    floor: 2,
+    period: is7Period
+      ? 7
+      : is8Period
+      ? 8
+      : is9Period
+      ? 9
+      : is10Period
+      ? 10
+      : 11,
+  },
 })
 
 export const ModalPage = atom<number>({ key: 'ModalPage', default: 1 })
