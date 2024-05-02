3526611206b17776448e74d41a81f2b86b1b68f5
이운린 <105215297+likegitman@users.noreply.github.com>
2024-05-02 10:41:33 +0900

    :art: Improve DI, SRP rules in Header

--- constants/headerObject.ts
+++ constants/headerObject.ts
@@ -1,7 +1,15 @@
-export const headerMenuList = [
-  { id: 1, text: '홈', link: '/' },
-  { id: 2, text: '공지', link: '/notice' },
-  { id: 3, text: '예약', link: '/reservation' },
-  { id: 4, text: '마이페이지', link: '/my-page' },
-  { id: 5, text: '학생정보', link: '/user' },
-]
+export const headerMenuList = {
+  0: [
+    { id: 1, text: '홈', link: '/' },
+    { id: 2, text: '공지', link: '/notice' },
+    { id: 3, text: '예약', link: '/reservation' },
+    { id: 4, text: '마이페이지', link: '/my-page' },
+  ],
+  1: [
+    { id: 1, text: '홈', link: '/' },
+    { id: 2, text: '공지', link: '/notice' },
+    { id: 3, text: '예약', link: '/reservation' },
+    { id: 4, text: '마이페이지', link: '/my-page' },
+    { id: 5, text: '학생정보', link: '/user' },
+  ],
+}
